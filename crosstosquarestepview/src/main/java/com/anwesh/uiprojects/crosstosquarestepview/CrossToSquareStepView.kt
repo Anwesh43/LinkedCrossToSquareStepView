bb2fbafb53065eb75ab4e1632035de7889627806
Anwesh43 <anweshthecool0@gmail.com>
2018-11-07 02:03:32 +0530

    adding Floar extension methods

--- crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
+++ crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
@@ -15,3 +15,13 @@ import android.app.Activity
 val nodes : Int = 5
 
 val lines : Int = 4
+
+val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / 0.5).toFloat()
+
+fun Float.updateScale(dir : Int) : Float = dir * scGap * (scaleFactor() + (1 - scaleFactor()) * lines.getInverse())
