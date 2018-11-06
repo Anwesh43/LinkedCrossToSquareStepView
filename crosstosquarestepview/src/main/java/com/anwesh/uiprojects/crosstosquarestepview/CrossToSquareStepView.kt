d36555732fb9d3c0027b498ac519866d6f9a807c
Anwesh43 <anweshthecool0@gmail.com>
2018-11-07 01:59:11 +0530

    adding libraries and defining constants

--- crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
+++ crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
@@ -3,3 +3,15 @@ package com.anwesh.uiprojects.crosstosquarestepview
 /**
  * Created by anweshmishra on 07/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+
+val nodes : Int = 5
+
+val lines : Int = 4
