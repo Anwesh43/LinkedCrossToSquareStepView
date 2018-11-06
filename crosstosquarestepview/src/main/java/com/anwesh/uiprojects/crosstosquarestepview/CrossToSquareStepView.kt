cc3cbd443b9373e24b8a2fc9e7de1d7a88b26436
Anwesh43 <anweshthecool0@gmail.com>
2018-11-07 02:12:07 +0530

    adding drawCTSNode canvas extension method

--- crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
+++ crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
@@ -25,3 +25,28 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.
 fun Float.scaleFactor() : Float = Math.floor(this / 0.5).toFloat()
 
 fun Float.updateScale(dir : Int) : Float = dir * scGap * (scaleFactor() + (1 - scaleFactor()) * lines.getInverse())
+
+fun Canvas.drawCTSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val size : Float = gap/3
+    paint.strokeWidth = Math.min(w, h) / 60
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#311B92")
+    save()
+    translate(gap * (i + 1), h / 2)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(i, lines)
+        save()
+        rotate(j * 90f)
+        val x : Float = (size/2) * (2 - sc2)
+        val y : Float = (size/2) * sc
+        drawLine(size * sc, 0f, size, 0f, paint)
+        drawLine(x, -y, x, y, paint)
+        restore()
+    }
+    restore()
+}
