aa8c713fe4ab732b32416e07f44fac805d60b774
Anwesh43 <anweshthecool0@gmail.com>
2018-11-07 09:41:25 +0530

    adding State class

--- crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
+++ crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
@@ -11,6 +11,7 @@ import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
 import android.app.Activity
+import android.util.Log
 
 val nodes : Int = 5
 
@@ -24,7 +25,7 @@ fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.
 
 fun Float.scaleFactor() : Float = Math.floor(this / 0.5).toFloat()
 
-fun Float.updateScale(dir : Int) : Float = dir * scGap * (scaleFactor() + (1 - scaleFactor()) * lines.getInverse())
+fun Float.updateScale(dir : Float) : Float = dir * scGap * (scaleFactor() + (1 - scaleFactor()) * lines.getInverse())
 
 fun Canvas.drawCTSNode(i : Int, scale : Float, paint : Paint) {
     val w : Float = width.toFloat()
@@ -66,5 +67,27 @@ class CrossToSquareStepView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            val k : Float = scale.updateScale(dir)
+            scale += k
+            Log.d("scale factor:", "$k")
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
