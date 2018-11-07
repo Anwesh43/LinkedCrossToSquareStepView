e077587dc1b11171d30bf055d3525186c5cb7eca
Anwesh43 <anweshthecool0@gmail.com>
2018-11-07 09:46:56 +0530

    adding CTSNode class

--- crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
+++ crosstosquarestepview/src/main/java/com/anwesh/uiprojects/crosstosquarestepview/CrossToSquareStepView.kt
@@ -118,4 +118,48 @@ class CrossToSquareStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CTSNode(var i : Int, val state : State = State()) {
+
+        private var next : CTSNode? = null
+        private var prev : CTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = CTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCTSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CTSNode {
+            var curr : CTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
