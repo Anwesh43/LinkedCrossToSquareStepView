d06a89db83a9cb6c6a707bd3911d3e1d7f2926e0
Anwesh43 <anweshthecool0@gmail.com>
2018-11-07 09:57:08 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedcrosstosquarestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedcrosstosquarestepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedcrosstosquarestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.crosstosquarestepview.CrossToSquareStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CrossToSquareStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
